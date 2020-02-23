ad99ca731303115b90553eb0d3dfd54cf8bc62bb
BenReasons <breasons@gmail.com>
2020-02-23 16:54:54 +0000

    test1

--- app.rb
+++ app.rb
@@ -9,6 +9,9 @@ before { puts "Parameters: #{params}" }
 # enter your Dark Sky API key here
 ForecastIO.api_key = "8ada1ea79ccb1b14962f899ce5000f24"
 
+# do the heavy lifting, use Global Hub lat/long
+forecast = ForecastIO.forecast(42.0574063,-87.6722787).to_hash
+
 get "/" do
     # show a view that asks for the location
     view "ask"
@@ -16,36 +19,14 @@ end
 
 get "/news" do
     # do everything else
-    results = Geocoder.search(params["q"])
+    results = Geocoder.search(params["location"])
     lat_long = results.first.coordinates # => [lat, long]
-    "#{lat_long[0]} #{lat_long[1]}"
+    lat = "#{lat_long[0]}"
+    long = "#{lat_long[1]}"
+    "#{lat} #{long}"
+
+    # url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=468f712111694cd8a42245681807ac81"
+    # news = HTTParty.get(url).parsed_response.to_hash
+    # current_news = news["articles"][3]["description"]
+    # "#{current_news}"
 end
-
-forecast = ForecastIO.forecast(#{lat_long[0]},#{lat_long[1]}).to_hash
-
-current_temperature = forecast["currently"]["summary"]
-
-print current_temperature
-
-# configure the Dark Sky API with your API key
-# ForecastIO.api_key = "8ada1ea79ccb1b14962f899ce5000f24"
-
-# do the heavy lifting, use Global Hub lat/long
-# forecast = ForecastIO.forecast(42.0574063,-87.6722787).to_hash
-
-# pp = pretty print
-# use instead of `puts` to make reading a hash a lot easier
-# e.g. `pp forecast`
-
-# current_temperature = forecast["currently"]["temperature"]
-# conditions = forecast["currently"]["summary"]
-
-# puts "In Evanston, it is currently #{current_temperature} and #{conditions}"
-
-# high_temperature = forecast["daily"]["data"][0]["temperatureHigh"]
-
-# puts high_temperature
-
-# for day in forecast["daily"]["data"]
-#     puts "A high temperature of #{day["temperatureHigh"]} and #{day["summary"]}"
-# end
