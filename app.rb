acd5de845f3bc04a6b11711621e46efacd1d1cc3
BenReasons <breasons@gmail.com>
2020-02-25 02:36:30 +0000

    test4

--- app.rb
+++ app.rb
@@ -29,7 +29,7 @@ get "/news" do
 
     @url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=468f712111694cd8a42245681807ac81"
     @news = HTTParty.get(@url).parsed_response.to_hash
-    @current_news = @news["articles"][3]["description"]
+    @current_news = @news["articles"]
 
 
     view "news"
--- test.rb
+++ test.rb
@@ -2,4 +2,6 @@ require "httparty"
 
 url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=468f712111694cd8a42245681807ac81"
 news = HTTParty.get(url).parsed_response.to_hash
-pp news["articles"][3]["description"]
+current_news = news["articles"]
+
+pp current_news
