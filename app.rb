7429d19a101c383c98b9377931a6da3e509fbd95
BenReasons <breasons@gmail.com>
2020-02-25 13:20:38 +0000

    test

--- app.rb
+++ app.rb
@@ -26,6 +26,8 @@ get "/news" do
     @current_temperature = @forecast["currently"]["temperature"]
     @current_conditions = @forecast["currently"]["summary"]
     @daily_forecast = @forecast["daily"]["data"].slice(0, 5)
+    # @day_time_base = @forecast["daily"]["data"][0]["time"]
+    # @day_time = Time.at(@day_time_base).strftime("%a").slice(0, 5)
 
     @url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=468f712111694cd8a42245681807ac81"
     @news = HTTParty.get(@url).parsed_response.to_hash
--- test2.rb
+++ test2.rb
@@ -4,17 +4,23 @@ require "forecast_io"
 ForecastIO.api_key = "8ada1ea79ccb1b14962f899ce5000f24"
 
 # do the heavy lifting, use Global Hub lat/long
-forecast = ForecastIO.forecast(42.0574063,-87.6722787).to_hash
+@forecast = ForecastIO.forecast(42.0574063,-87.6722787).to_hash
 
 # pp = pretty print
 # use instead of `puts` to make reading a hash a lot easier
 # e.g. `pp forecast`
-current_temperature = forecast["currently"]["temperature"]
-conditions = forecast["currently"]["summary"]
-puts "In Evanston, it is currently #{current_temperature} and #{conditions}"
+# current_temperature = forecast["currently"]["temperature"]
+# conditions = forecast["currently"]["summary"]
+# puts "In Evanston, it is currently #{current_temperature} and #{conditions}"
 # high_temperature = forecast["daily"]["data"][0]["temperatureHigh"]
 # puts high_temperature
 # puts forecast["daily"]["data"][1]["temperatureHigh"]
 # puts forecast["daily"]["data"][2]["temperatureHigh"]
 
-pp forecast["currently"]
+@day = @forecast["daily"]["data"][0]["time"]
+@day1 = Time.at(@day).strftime("%a")
+
+@day_time_base = @forecast["daily"]["data"][0]["time"]
+@day_time = Time.at(@day_time_base).strftime("%a").slice(0, 5)
+
+pp @day_time
